7a219621a467b4a71de70f77e469a938bd71f8d5
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-09-04 23:28:42 +0530

    front end started

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutLineEdit } from "react-icons/ai";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
