ac73d6f3652fb0390bc67e604bed89094c650cbc
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-09-11 23:13:28 +0530

    Delete fixed

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://book-store-mern-stack.vercel.app/${id}`)
+      .delete(`https://book-store-mern-stack.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Deleted successfull", { variant: "success" });
